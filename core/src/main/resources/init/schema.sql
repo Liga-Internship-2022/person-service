500fc4071502e530ea3181c583de237e83c43b53
Kirill Kainov <kainov.dev@gmail.com>
2022-10-21 21:21:46 +0300

    add schema.sql

---
+++ core/src/main/resources/init/schema.sql
@@ -0,0 +1,51 @@
+create table if not exists medical_card
+(
+    id            bigserial not null unique primary key,
+    client_status char      not null,
+    med_status    char      not null,
+    registry_dt   date      not null,
+    comment       text      not null
+);
+
+create table if not exists illness
+(
+    id              bigserial not null unique primary key,
+    medical_card_id bigint    not null references medical_card (id),
+    type_id         bigint    not null,
+    heaviness       char      not null,
+    appearance_dttm timestamp not null,
+    recovery_dt     date      not null
+);
+
+create table if not exists contact
+(
+    id           bigserial    not null unique primary key,
+    phone_number varchar(32)  not null,
+    email        varchar(128) not null,
+    profile_link text         not null
+);
+
+create table if not exists person_data
+(
+    id              bigserial    not null unique primary key,
+    last_name       varchar(255) not null,
+    first_name      varchar(255) not null,
+    birth_dt        date         not null,
+    age             smallint     not null,
+    sex             char         not null,
+    contact_id      bigint       not null references contact (id),
+    medical_card_id bigint       not null references medical_card (id),
+    parent_id       bigint       not null check ( parent_id <> person_data.id ) references person_data (id)
+);
+
+create table if not exists address
+(
+    id         bigserial    not null unique primary key,
+    contact_id bigint       not null references contact (id),
+    country_id bigint       not null,
+    city       varchar(255) not null,
+    index      integer      not null,
+    street     varchar(255) not null,
+    building   varchar(32)  not null,
+    flat       varchar(32)  not null
+);
