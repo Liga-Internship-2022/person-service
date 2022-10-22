cdc6e3f15b871102ba6474453364c154d7b83513
Kirill Kainov <kainov.dev@gmail.com>
2022-10-22 08:57:16 +0300

    minor change schema and fill with test data

--- core/src/main/resources/init/schema.sql
+++ core/src/main/resources/init/schema.sql
@@ -7,6 +7,14 @@ create table if not exists medical_card
     comment       text      not null
 );
 
+create table if not exists contact
+(
+    id           bigserial    not null unique primary key,
+    phone_number varchar(32)  not null,
+    email        varchar(128) not null,
+    profile_link text         not null
+);
+
 create table if not exists illness
 (
     id              bigserial not null unique primary key,
@@ -15,14 +23,6 @@ create table if not exists illness
     heaviness       char      not null,
     appearance_dttm timestamp not null,
     recovery_dt     date      not null
-);
-
-create table if not exists contact
-(
-    id           bigserial    not null unique primary key,
-    phone_number varchar(32)  not null,
-    email        varchar(128) not null,
-    profile_link text         not null
 );
 
 create table if not exists person_data
@@ -35,7 +35,7 @@ create table if not exists person_data
     sex             char         not null,
     contact_id      bigint       not null references contact (id),
     medical_card_id bigint       not null references medical_card (id),
-    parent_id       bigint       not null check ( parent_id <> person_data.id ) references person_data (id)
+    parent_id       bigint check ( parent_id <> person_data.id ) references person_data (id)
 );
 
 create table if not exists address
