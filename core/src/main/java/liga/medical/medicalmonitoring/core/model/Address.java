29e1ce6f4c2b4b88d0ea4a082bc32d1d2c7dfc52
Kirill Kainov <kainov.dev@gmail.com>
2022-11-02 11:01:27 +0300

    fix codestyle

--- core/src/main/java/liga/medical/medicalmonitoring/core/model/Address.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/model/Address.java
@@ -2,10 +2,7 @@ package liga.medical.medicalmonitoring.core.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
