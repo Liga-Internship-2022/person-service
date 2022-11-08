047922bc46c98b4e545d9ce25395baad047e208c
Kirill Kainov <kainov.dev@gmail.com>
2022-11-08 23:22:29 +0300

    refactor logging aop

--- core/src/main/java/liga/medical/medicalmonitoring/core/aop/LoggingAdvice.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/aop/LoggingAdvice.java
@@ -2,11 +2,13 @@ package liga.medical.medicalmonitoring.core.aop;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import liga.medical.service.LoggingService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import model.LogMessage;
+import model.SystemType;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,25 +16,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LoggingAdvice {
+
+    private final LoggingService loggingService;
 
     @Pointcut(value = "@annotation(liga.medical.medicalmonitoring.core.aop.annotations.RestLog)")
     public void restLogPointcut() {
     }
 
-    @Around(value = "restLogPointcut()")
-    public Object restLogger(ProceedingJoinPoint pjp) throws JsonProcessingException {
+    @Before(value = "restLogPointcut()")
+    public void restLogger(JoinPoint jp) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String authName = auth.getName();
-        log.info("auth.getName() = {}", authName);
-
         Object principal = auth.getPrincipal();
-
         String username;
         if (principal instanceof UserDetails) {
             username = ((UserDetails) principal).getUsername();
@@ -41,21 +40,17 @@ public class LoggingAdvice {
         }
 
         String logId = "rest-logger";
-        String className = pjp.getTarget().getClass().getName();
-        String methodName = pjp.getSignature().getName();
-        String args = objectMapper.writeValueAsString(pjp.getArgs());
+        String className = jp.getTarget().getClass().getName();
+        String methodName = jp.getSignature().getName();
+        String args = objectMapper.writeValueAsString(jp.getArgs());
 
-        log.info("[{}] пользователем [{}] из класса {} вызван метод {}() с аргументами {}",
-                logId, username, className, methodName, args);
-
-        Object object = null;
-        try {
-            object = pjp.proceed();
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        }
-
-        log.info("[{}] {}: {}() response -> [{}]", logId, className, methodName, objectMapper.writeValueAsString(object));
-        return object;
+        LogMessage logMessage = LogMessage.builder()
+                .logId(logId)
+                .username(username)
+                .className(className)
+                .methodName(methodName)
+                .args(args)
+                .build();
+        loggingService.log(logMessage, SystemType.PERSON_SERVICE);
     }
 }
